1a660e39a195df16b03354ae1fb6941426843663
Paola1907 <90858694+Paola1907@users.noreply.github.com>
2021-09-20 22:50:09 -0500

    Update bienvenida.js

--- js/bienvenida.js
+++ js/bienvenida.js
@@ -3,7 +3,10 @@ monogatari.script ({
 	'Start_Bienvenida': [
   
 		'show character t feliz at left with fadeIn',	
-		'Esta es la bienvenida...',
+		't: Querido aprendiz, bienvenido a este mundo de aprendizaje interactivo,',
+                't: en donde te sumergirás a aprender de una manera diferente, pues tus conocimientos serán tu mecanismo de defensa para lograr la meta.',
+                't: Mi nombre es Tereis y estaré acompañándote en toda esta travesía en donde el que tiene el conocimiento tiene el poder, así que adelante!',
+
 		'end',
   
 	],
