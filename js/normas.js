9523819877ac8a9d817f0d41cd9fc618e8aeb6e1
Kellypadi <90870209+Kellypadi@users.noreply.github.com>
2021-11-09 16:03:29 -0500

    Update normas.js

--- js/normas.js
+++ js/normas.js
@@ -2,7 +2,7 @@ monogatari.script ({
 
   'Start_Normas': [
 
-    'Estimado Aprendiz'
+    'Estimado Aprendiz,',
     'Para  conocer la normativa comercial, contable y financiera vigente,',
     'su objetivo e identificar el organismo emisor usted debe,',
     'responder los siguientes interrogantes para evidencia  de su conocimiento,',
