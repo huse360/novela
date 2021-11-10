1081260aa735844f63850223ec52b43e3700c93f
Kellypadi <90870209+Kellypadi@users.noreply.github.com>
2021-11-10 12:08:50 -0500

    Update normas.js

--- js/normas.js
+++ js/normas.js
@@ -4,19 +4,28 @@ monogatari.script ({
 
     'Estimado Aprendiz,',
     'La globalización y el ritmo actual del mundo exigen, cada vez con mayor fuerza, un idioma universal,',
-    'sobre todo en cuestiones de negocios y finanzas,',
+    'sobre todo en cuestiones de negocios y finanzas',
     'La información confiable y comparable en el ámbito financiero,', 
-    'es necesaria para que en las empresas se tomen las mejores decisiones,',
+    'es necesaria para que en las empresas se tomen las mejores decisiones',
     'Para  conocer la normativa comercial, contable y financiera vigente,',
     'es necesario que usted revise el contenido tematico,',
     'Para dar respuesta a los siguientes interrogantes y evidenciar el conocimiento adquirido,',
-    
-    'Aprendiz, una vez constituida su empresa selecciones el paso a seguir,',
-    
-    'Definir el marco normativo contable y financiero aplicable,',
-    'Identificar si de acuerdo a la normativa vigente en Colombia, está obligada a llevar contabilidad según NIIF,',
-    'Conocer la actividad comercial y necesidades de la empresa,',
-
+    ],
+  'Elección':[
+    {
+      Choice':{
+      'Dialog': 't:¿Aprendiz, una vez constituida su empresa selecciones el paso a seguir,',
+      'p1':{
+      'Text':'Definir el marco normativo contable y financiero aplicable',
+      'Do':'jump Definirelmarconormativocontableyfinancieroaplicable'  
+    },
+    'p2:{
+    'Text': 'Conocer la actividad comercial y necesidades de la empresa',
+    },
+    'p3:{
+    'Text':'Identificar si de acuerdo a la normativa vigente en Colombia, está obligada a llevar contabilidad según NIIF',
+    },
+   
 
     'end',
 
