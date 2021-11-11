3ad25a18c35f082f2cf6c0a0a059e9d15a2a8ffd
Kellypadi <90870209+Kellypadi@users.noreply.github.com>
2021-11-10 21:37:30 -0500

    Update normas.js

--- js/normas.js
+++ js/normas.js
@@ -6,31 +6,7 @@ monogatari.script ({
 			//'jump Conocerlaactividadcomercialynecesidadesdelaempresa',  
 			'show character t normal',
 			'jump Eleccion',
-
-    ],
-  
-  'Elección':[
-    {
-      Choice':{
-      'Dialog': 't: ¿Aprendiz, una vez constituida su empresa selecciones el paso a seguir?',
-      'p1':{
-      'Text':'Identificar el marco normativo contable',
-      'Do':'jump Definirelmarconormativocontable'  
-    },
-    'p2:{
-    'Text': 'Conocer la actividad comercial de la empresa',
-    'Do':'Conocerlaactividadcomercialdelaempresa'
-    },
-    'p3:{
-    'Text':'Verificar si está obligada a llevar contabilidad según NIIF',
-    'Do':'VerificarsiestáobligadaallevarcontabilidadsegúnNIIF'             
-    },
-  
-}
-},
-],
-
-   
+ 
 
     'end',
 
