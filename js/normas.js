de9bd972774efc75525c6168e9e7cc8e6635a465
Kellypadi <90870209+Kellypadi@users.noreply.github.com>
2021-11-15 08:30:55 -0500

    Update normas.js

--- js/normas.js
+++ js/normas.js
@@ -19,14 +19,14 @@ monogatari.script ({
     ],
 'Inicio': [
     
-    't:'Estimado Aprendiz,',
-    't:'La globalización y el ritmo actual del mundo exigen, cada vez con mayor fuerza, un idioma universal,',
-    't:'sobre todo en cuestiones de negocios y finanzas.',
-    't:'La información confiable y comparable en el ámbito financiero,', 
-    't:'es necesaria para que en las empresas se tomen las mejores decisiones.',
-    't:'Para  conocer la normativa comercial, contable y financiera vigente,',
-    't:'es necesario que usted revise el contenido tematico.',
-    't:'Para dar respuesta a los siguientes interrogantes y evidenciar el conocimiento adquirido,',
+    't: 'Estimado Aprendiz,',
+    't: 'La globalización y el ritmo actual del mundo exigen, cada vez con mayor fuerza, un idioma universal,',
+    't: 'sobre todo en cuestiones de negocios y finanzas.',
+    't: 'La información confiable y comparable en el ámbito financiero,', 
+    't: 'es necesaria para que en las empresas se tomen las mejores decisiones.',
+    't: 'Para  conocer la normativa comercial, contable y financiera vigente,',
+    't: 'es necesario que usted revise el contenido tematico.',
+    't: 'Para dar respuesta a los siguientes interrogantes y evidenciar el conocimiento adquirido,',
   ]
 
 
