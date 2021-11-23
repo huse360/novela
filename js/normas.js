55f7afd8ad9c165566811b68479d5f45ee06a5a5
Kellypadi <90870209+Kellypadi@users.noreply.github.com>
2021-11-23 07:54:40 -0500

    Update normas.js

--- js/normas.js
+++ js/normas.js
@@ -46,6 +46,8 @@ monogatari.script ({
        't: Una vez constituida su empresa el siguiente paso es definir el marco normativo contable y financiero aplicable,',
        't: por lo anterior debe identificar si de acuerdo a la normativa vigente en Colombia,',
        't: su empresa está obligada a llevar contabilidad según NIIF',
+     
+       'end', 
              ],
   
    'Politicas contables': [
@@ -54,6 +56,8 @@ monogatari.script ({
        't: Ya tiene identificada la estructura contable de su  empresa,',
        't: el siguiente paso es conocer las normas a tener en cuenta para la selección y',
        't: aplicación de las políticas contables  conforme a los lineamientos de las NIIF para PYMES',
+     
+       'end', 
              ], 
 
   
