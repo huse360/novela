69590b6978c8f0b65f8c93646d4f5568723a1b89
Kellypadi <90870209+Kellypadi@users.noreply.github.com>
2021-11-23 06:45:30 -0500

    Update normas.js

--- js/normas.js
+++ js/normas.js
@@ -15,63 +15,14 @@ monogatari.script ({
     't: para dar respuesta a los siguientes interrogantes y evidenciar el conocimiento adquirido,',
 
     
-    {
-       'Choice': {
-         
-           'Dialog': 't: normativa, ¿NIIF? ¿DUR 2420', 
-         
-           'p1': {
-             'Text': 'Normativa',
-             'Do': 'jump Normativa'
-           },
-         
-           'p2': {
-             'Text': 'Politicas',
-             'Do': 'jump Politicas'
-           },
+    
 
-       } 
-    },
-    
-    
-    'show character t feliz at left with fadeIn',
- 't: Soy Teresa y ahora estoy feliz.',
- 'show character t brava at center',
- 't: ¡Pero también me puedo poner brava!',
-
- 'show character t triste at right',
- 't: o triste...',
- 'show character t cansada at left',
- 't: ...pero ya me cansé...',
- 'hide character t cansada with fadeOut',
- 't: Y me voy.',
- 't: Chao.', 
-    
-    
-    
-    
-    'show character t brava at center with fadeIn', 
-    't: Estimado Aprendiz,',
-    
-   't: La globalización y el ritmo actual del mundo exigen, cada vez con mayor fuerza, un idioma universal,',
-    'sobre todo en cuestiones de negocios y finanzas.',
-    'La información confiable y comparable en el ámbito financiero,', 
-    'es necesaria para que en las empresas se tomen las mejores decisiones.',
-    'Para  conocer la normativa comercial, contable y financiera vigente,',
-    'es necesario que usted revise el contenido tematico,',
-    'para dar respuesta a los siguientes interrogantes y evidenciar el conocimiento adquirido,',
+       
 
     'end',
     
   ],
   
-  'Normativa': [
-       'escogiste la normativa',
-  ],
-  
-   'Politicas': [
-       'escogiste la politicas',
-  ], 
   
   
   
